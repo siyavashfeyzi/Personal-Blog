9c98bce490e666df15de5564480f7108211b2140
siyavash <feyzisiyavash@gmail.com>
2023-11-26 16:52:42 +0330

    fix bug

--- Blog/views.py
+++ Blog/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return render(request, 'index.html', {'name': "SIyavash"})
+    return render(request, 'index.html', {'name': "Siyavash"})
