916cded6f18329628e2c68c8a1036a74b411e700
siyavash <feyzisiyavash@gmail.com>
2023-11-27 14:41:08 +0330

    Add debug_toolbar to the project

--- PersonalBlog/urls.py
+++ PersonalBlog/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("", include("Blog.urls")),
     path("admin/", admin.site.urls),
+    path("", include("Blog.urls")),
+    path("__debug__/", include("debug_toolbar.urls")),
 ]
