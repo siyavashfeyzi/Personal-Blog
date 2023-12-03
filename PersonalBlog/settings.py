42e52f11de7dc4b0bd16ba86e41f31867fa6b5bc
siyavash <feyzisiyavash@gmail.com>
2023-12-03 15:28:46 +0330

    add media setting

--- PersonalBlog/settings.py
+++ PersonalBlog/settings.py
@@ -131,6 +131,9 @@ STATIC_URL = "/static/"
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "Blog/static")
 ]
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
