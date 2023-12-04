5eec42a5f708e447cd0a99dc4595a10c4854bbdf
siyavash <feyzisiyavash@gmail.com>
2023-12-04 14:05:46 +0330

    Add Tag model + Generic Realationship

--- Tags/models.py
+++ Tags/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
 
-# Create your models here.
+
+class Tag(models.Model):
+    lable = models.CharField(max_length=255)
+
+
+# What tag applied to what object.
+class TaggedItem(models.Model):
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
+
+    # Generic Realationship
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey()
